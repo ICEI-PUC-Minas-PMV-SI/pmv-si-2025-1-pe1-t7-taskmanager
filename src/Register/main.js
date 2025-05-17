9d96316171c7687d8e51d3b66013722daba27902
Davi Sobrinho <davi.sobrinho@seidor.com>
2025-05-16 21:02:33 -0300

    adiciona usuarios no localStorage

---
+++ src/Register/main.js
@@ -0,0 +1,35 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('usuario').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value;
+    const confirmarSenha = document.getElementById('confirmar-senha').value;
+
+    if (senha !== confirmarSenha) {
+        alert('As senhas não coincidem.');
+        return;
+    }
+
+    const novoUsuario = {
+        nome,
+        email,
+        senha,
+    };
+
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    usuarios.push(novoUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    alert('Usuário cadastrado com sucesso!');
+
+    window.location.href = '../login/index.html';
+    form.reset();
+    });
+});
---
+++ src/login/main.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+    
+        const usuario = document.getElementById('username').value.trim();
+        const senha = document.getElementById('password').value;
+
+        const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+        const usarioEncontrado = usuarios.find(user => user.nome === usuario)
+    
+        if (usarioEncontrado.senha === senha) {
+            localStorage.setItem('usuario', JSON.stringify(usarioEncontrado));
+            alert('Usuário logado com sucesso!');
+            window.location.href = '../front_page/index.html';
+
+        } else {
+            alert('Senha incorreta!');
+        }
+
+        form.reset();
+    });
+});
+    
