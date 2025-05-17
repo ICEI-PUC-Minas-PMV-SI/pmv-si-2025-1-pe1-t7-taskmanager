9ac487b65942c7d599973acc2bc6db2dddb8ea91
Davi Sobrinho <davi.sobrinho@seidor.com>
2025-05-16 21:06:19 -0300

    identa codigo de login

--- src/Register/main.js
+++ src/Register/main.js
@@ -2,34 +2,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 const form = document.querySelector('form');
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
 
-    const nome = document.getElementById('usuario').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const senha = document.getElementById('senha').value;
-    const confirmarSenha = document.getElementById('confirmar-senha').value;
+        const nome = document.getElementById('usuario').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const senha = document.getElementById('senha').value;
+        const confirmarSenha = document.getElementById('confirmar-senha').value;
 
-    if (senha !== confirmarSenha) {
-        alert('As senhas não coincidem.');
-        return;
-    }
+        if (senha !== confirmarSenha) {
+            alert('As senhas não coincidem.');
+            return;
+        }
 
-    const novoUsuario = {
-        nome,
-        email,
-        senha,
-    };
+        const novoUsuario = {
+            nome,
+            email,
+            senha,
+        };
 
-    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+        const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-    usuarios.push(novoUsuario);
+        usuarios.push(novoUsuario);
 
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
 
-    alert('Usuário cadastrado com sucesso!');
+        alert('Usuário cadastrado com sucesso!');
 
-    window.location.href = '../login/index.html';
-    form.reset();
+        window.location.href = '../login/index.html';
+        form.reset();
     });
 });
